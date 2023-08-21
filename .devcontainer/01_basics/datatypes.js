a748eba8521ffa0f5f6251f2c2da69714f4ac091
Dthakur-12 <107418318+Dthakur-12@users.noreply.github.com>
2023-08-21 07:49:32 +0000

    uppdated

--- .devcontainer/01_basics/datatypes.js
+++ .devcontainer/01_basics/datatypes.js
@@ -1,6 +1,37 @@
-let name='string'
-let age=23
-let isloogesin=true
-console.log(typeof 'durgesh');
-console.log(typeof null);
-console.log(typeof 45);
+// let name='string'
+// let age=23
+// let isloogesin=true
+// console.log(typeof 'durgesh');
+// console.log(typeof null);
+// console.log(typeof 45);
+// let value='33'
+// let newValue=Number(value);
+// console.log(typeof newValue)
+
+// let value=3;
+// let negvalue=-value;
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%7);
+// let str1='hello'
+// let str2='hitesh'
+// let str3=str1+ " " +str2;
+// console.log(str3);
+// console.log("1"+3);
+// console.log(1+"3");
+// console.log("1"+3+4);
+// console.log(1+4+"3");
+// console.log(+true);
+// let gameCounter=100;
+// gameCounter++;
+// console.log(gameCounter);
+// ++gameCounter;
+// console.log(gameCounter);
+
+console.log(2!=1);
+console.log("2">1);
+console.log("02">1);
--- .devcontainer/01_basics/variables.js
+++ .devcontainer/01_basics/variables.js
@@ -1,6 +1,6 @@
 const accountId=144566
 let acccountEmail="Durgesh@gmail"
 var accountPassword='12345'
-const newName;
+// const newName;
 
 console.table([accountId,acccountEmail,accountPassword,newName])
